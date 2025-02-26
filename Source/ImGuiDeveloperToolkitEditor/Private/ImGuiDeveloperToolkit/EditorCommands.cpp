9af1ae3330547cff7360c9818d8f86d718780d7b
Mikołaj Radwan <me@mikolajradwan.pl>
2025-02-26 23:26:44 +0100

    Added missing files for property inspector

---
+++ Source/ImGuiDeveloperToolkitEditor/Private/ImGuiDeveloperToolkit/EditorCommands.cpp
@@ -0,0 +1,107 @@
+﻿#include "EditorCommands.h"
+
+#include "ImGuiDeveloperToolkit/ImGuiDeveloperToolkitSubsystem.h"
+#include "LevelEditor.h"
+
+#define LOCTEXT_NAMESPACE "EditorCommands"
+
+namespace ImGuiDeveloperToolkit
+{
+
+namespace Private
+{
+
+class FEditorCommands final : public TCommands<FEditorCommands>
+{
+public:
+	FEditorCommands()
+		: TCommands{
+			  TEXT("DeveloperToolkitEditorCommands"),
+			  FText::FromString("DeveloperToolkitEditorCommands"),
+			  NAME_None,
+			  FAppStyle::GetAppStyleSetName()}
+	{
+	}
+
+	virtual void RegisterCommands() override;
+
+	TSharedPtr<FUICommandInfo> ShowDeveloperToolkitCommand = nullptr;
+};
+
+void FEditorCommands::RegisterCommands()
+{
+	UI_COMMAND(
+		ShowDeveloperToolkitCommand,
+		"Show Developer Toolkit",
+		"Show the Developer Toolkit window",
+		EUserInterfaceActionType::ToggleButton,
+		FInputChord{EKeys::Backslash});
+}
+
+UImGuiDeveloperToolkitSubsystem* GetEditorImGuiSubsystem()
+{
+	if (!IsValid(GEngine))
+	{
+		return nullptr;
+	}
+
+	return GEngine->GetEngineSubsystem<UImGuiDeveloperToolkitSubsystem>();
+}
+
+}  // namespace Private
+
+TSharedRef<FUICommandList> RegisterEditorCommands()
+{
+	Private::FEditorCommands::Register();
+	TSharedRef<FUICommandList> CommandList = MakeShared<FUICommandList>();
+
+	const Private::FEditorCommands& Commands = Private::FEditorCommands::Get();
+	CommandList->MapAction(
+		Commands.ShowDeveloperToolkitCommand,
+		FExecuteAction::CreateLambda(
+			[]
+			{
+				UImGuiDeveloperToolkitSubsystem* const ImGuiSubsystem = Private::GetEditorImGuiSubsystem();
+				if (!IsValid(ImGuiSubsystem))
+				{
+					return;
+				}
+
+				ImGuiSubsystem->ToggleShow();
+			}),
+		FCanExecuteAction{},
+		FIsActionChecked::CreateLambda(
+			[]
+			{
+				const UImGuiDeveloperToolkitSubsystem* const ImGuiSubsystem = Private::GetEditorImGuiSubsystem();
+				if (!IsValid(ImGuiSubsystem))
+				{
+					return false;
+				}
+
+				return ImGuiSubsystem->IsShow();
+			}));
+
+	FLevelEditorModule& LevelEditorModule = FModuleManager::LoadModuleChecked<FLevelEditorModule>("LevelEditor");
+	TSharedRef<FExtender> MenuExtender = MakeShared<FExtender>();
+	MenuExtender->AddMenuExtension(
+		"WindowLayout",
+		EExtensionHook::After,
+		CommandList,
+		FMenuExtensionDelegate::CreateLambda(
+			[](FMenuBuilder& MenuBuilder)
+			{ MenuBuilder.AddMenuEntry(Private::FEditorCommands::Get().ShowDeveloperToolkitCommand); }));
+
+	LevelEditorModule.GetMenuExtensibilityManager()->AddExtender(MenuExtender);
+
+	return CommandList;
+}
+
+void UnregisterEditorCommands()
+{
+	Private::FEditorCommands::Unregister();
+}
+
+}  // namespace ImGuiDeveloperToolkit
+
+#undef LOCTEXT_NAMESPACE
---
+++ Source/ImGuiDeveloperToolkitWidgets/Private/ImGuiDeveloperToolkit/PropertyInspector.cpp
@@ -0,0 +1,166 @@
+﻿#include "ImGuiDeveloperToolkit/PropertyInspector.h"
+
+#include "Containers/AnsiString.h"
+#include "imgui.h"
+
+namespace ImGuiDeveloperToolkit::PropertyInspector
+{
+
+namespace Private
+{
+
+void Inspect(const char* Label, const FProperty& Property, void* Outer);
+
+void Inspect(const char* Label, const FIntProperty& IntProperty, void* Outer)
+{
+	ImGui::TableNextRow();
+
+	ImGui::TableNextColumn();
+	ImGui::Text("%s", Label);
+
+	ImGui::TableNextColumn();
+
+	int32* Ptr = IntProperty.ContainerPtrToValuePtr<int32>(Outer);
+	int32 Value = IntProperty.GetPropertyValue(Ptr);
+	ImGui::PushID(Label);
+	if (ImGui::InputInt("", &Value))
+	{
+		IntProperty.SetPropertyValue(Ptr, Value);
+		// make sure property changed gets propagated, maybe even transact?
+	}
+	ImGui::PopID();
+}
+
+void Inspect(const char* Label, const FArrayProperty& ArrayProperty, void* Outer)
+{
+	void* ArrayData = ArrayProperty.ContainerPtrToValuePtr<void>(Outer);
+
+	if (ArrayData == nullptr || ArrayProperty.Inner == nullptr)
+	{
+		return;
+	}
+
+	FScriptArrayHelper ArrayHelper{&ArrayProperty, ArrayData};
+
+	const int32 NumElements = ArrayHelper.Num();
+
+	ImGui::TableNextRow();
+	ImGui::TableNextColumn();
+	const bool bShowElements = ImGui::TreeNode(Label);
+	ImGui::TableNextColumn();
+	ImGui::Text("[%d]", NumElements);
+
+	if (!bShowElements)
+	{
+		return;
+	}
+
+	ImGui::PushID(Label);
+
+	for (int32 Index = 0; Index < NumElements; ++Index)
+	{
+		FAnsiStringBuilderBase LabelBuilder;
+		LabelBuilder.Append("[").Append(FAnsiString::FromInt(Index)).Append("]");
+
+		Inspect(LabelBuilder.ToString(), *ArrayProperty.Inner, ArrayHelper.GetRawPtr(Index));
+	}
+
+	ImGui::PopID();
+
+	ImGui::TreePop();
+}
+
+void Inspect(const char* Label, const UStruct& Struct, void* Instance)
+{
+	ImGui::TableNextRow();
+	ImGui::TableNextColumn();
+	const bool bShowMembers = ImGui::TreeNode(Label);
+	ImGui::TableNextColumn();
+
+	const char* const TypeDisplayName =
+		reinterpret_cast<const char*>(StringCast<UTF8CHAR>(*Struct.GetDisplayNameText().ToString()).Get());
+	ImGui::Text("{%s}", TypeDisplayName);
+
+	if (!bShowMembers)
+	{
+		return;
+	}
+
+	for (TFieldIterator<FProperty> It(&Struct); It; ++It)
+	{
+		const FProperty* const Property = *It;
+
+		if (!Property)
+		{
+			continue;
+		}
+
+		const char* const PropertyName =
+			reinterpret_cast<const char*>(StringCast<UTF8CHAR>(*Property->GetName()).Get());
+		const char* const DisplayName =
+			reinterpret_cast<const char*>(StringCast<UTF8CHAR>(*Property->GetDisplayNameText().ToString()).Get());
+
+		ImGui::PushID(PropertyName);
+		Inspect(DisplayName, *Property, Instance);
+		ImGui::PopID();
+	}
+
+	ImGui::TreePop();
+}
+
+void Inspect(const char* Label, const FStructProperty& StructProperty, void* Outer)
+{
+	if (!IsValid(StructProperty.Struct))
+	{
+		return;
+	}
+
+	const char* const PropertyName =
+		reinterpret_cast<const char*>(StringCast<UTF8CHAR>(*StructProperty.GetName()).Get());
+	const char* const DisplayName =
+		reinterpret_cast<const char*>(StringCast<UTF8CHAR>(*StructProperty.GetDisplayNameText().ToString()).Get());
+
+	ImGui::PushID(PropertyName);
+	Inspect(DisplayName, *StructProperty.Struct, StructProperty.ContainerPtrToValuePtr<void>(Outer));
+	ImGui::PopID();
+}
+
+void Inspect(const char* Label, const FProperty& Property, void* Outer)
+{
+	if (const FArrayProperty* const ArrayProperty = CastField<FArrayProperty>(&Property))
+	{
+		Inspect(Label, *ArrayProperty, Outer);
+	}
+	if (const FIntProperty* const IntProperty = CastField<FIntProperty>(&Property))
+	{
+		Inspect(Label, *IntProperty, Outer);
+	}
+	if (const FStructProperty* const StructProperty = CastField<FStructProperty>(&Property))
+	{
+		Inspect(Label, *StructProperty, Outer);
+	}
+}
+
+}  // namespace Private
+
+void Inspect(const char* Label, const UStruct& Struct, void* Instance)
+{
+	if (Instance == nullptr)
+	{
+		return;
+	}
+
+	constexpr int TableFlags = ImGuiTableFlags_BordersV | ImGuiTableFlags_BordersOuterH | ImGuiTableFlags_Resizable
+							   | ImGuiTableFlags_RowBg | ImGuiTableFlags_NoBordersInBody;
+	if (ImGui::BeginTable("Struct", 2, TableFlags))
+	{
+		ImGui::TableSetupColumn("Index" /*, ImGuiTableColumnFlags_WidthFixed | ImGuiTableColumnFlags_NoHide*/);
+		ImGui::TableSetupColumn("Value", ImGuiTableColumnFlags_WidthStretch);
+
+		Private::Inspect(Label, Struct, Instance);
+
+		ImGui::EndTable();
+	}
+}
+
+}  // namespace ImGuiDeveloperToolkit::PropertyInspector
